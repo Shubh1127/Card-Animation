c79dcc489e58f5142c43f920bb0327d0a0e7bb08
shubham <shubhamsinghmor2312@gmail.com>
2024-09-26 20:45:50 +0530

    changes done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Image from './Image'
+function App() {
+  
+
+  return (
+    <>
+      <Image/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Image.jsx
@@ -0,0 +1,22 @@
+import './Style.css'
+export default function Image( ){
+    return (
+        <div className="cont">
+        <div className="img">
+            <img src="/p1 (1).jpg" alt="p1"/>
+        </div>
+        <div className="img">
+            <img src="/p2.jpg" alt="p1"/>
+        </div>
+        <div className="img">
+            <img src="/p3.jpg" alt="p1"/>
+        </div>
+        <div className="img">
+            <img src="/p4.jpg" alt="p1"/>
+        </div>
+        <div className="img">
+            <img src="/p5.jpg" alt="p1"/>
+        </div>
+    </div>
+    )
+}
